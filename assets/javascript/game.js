d69537917b8c2f8bd88b3be978120d225babff5a
sahasra101 <ajaykiri7@gmail.com>
2019-10-21 22:51:54 -0400

    wrote additional functions to win lose reset game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,5 @@
-
-
 // function to code what each crystal button does
 jQuery(document).ready(function ($) {
-
 
     var blueN = 0;
     var greenN = 0;
@@ -17,10 +14,10 @@ jQuery(document).ready(function ($) {
     //    set new random numbers for all 4 buttons and goalNumbeer
     function resetGame() {
         console.log(`this is inside function to reset game`)
-        blueN = Math.floor(Math.random() * 13);
-        greenN = Math.floor(Math.random() * 13);
-        yellowN = Math.floor(Math.random() * 13);
-        redN = Math.floor(Math.random() * 13);
+        blueN = Math.floor(Math.random() * 12)  + 1;
+        greenN = Math.floor(Math.random() * 12) + 1;
+        yellowN = Math.floor(Math.random() * 12) + 1;
+        redN = Math.floor(Math.random() * 12) + 1;
         workingTotal = 0;
         goalNumber = Math.floor(Math.random() * 101) + 19;
     }
@@ -28,12 +25,29 @@ jQuery(document).ready(function ($) {
     // function to play 
     function play(color) {
         console.log(`You pressed the ${color} crystal.`);
-        workingTotal =+ color;
+        workingTotal = workingTotal + color;
             }
 
     // function to compare working total to goal number
     function compare() {
         console.log(`Working total is: ${workingTotal} and goal number is ${goalNumber}`);
+        if (goalNumber === workingTotal){
+            winGame();
+        } else if (goalNumber < workingTotal) {
+            loseGame();
+        }
+    }
+
+    function winGame() {
+        wins++;
+        console.log(`you have won the game!! Now your score is ${wins}`);
+        resetGame();
+    }
+    
+    function loseGame() {
+        losses++;
+        console.log(`you have lost the game!! Now your losses are ${losses}`);
+        resetGame();
     }
 
     resetGame();
